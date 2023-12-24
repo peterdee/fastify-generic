128e59dacb4029a5cd58800744f19d83d8323157
Peter <peter.dyumin@gmail.com>
2023-12-24 21:01:47 +0300

    Work on apiDoc

--- apis/index/controller.js
+++ apis/index/controller.js
@@ -2,19 +2,6 @@ import response from '../../utilities/response.js';
 import '../../types.js';
 
 /**
- * @api {get} / Index route
- * @apiGroup Index
- * @apiName index
- * @apiSampleRequest /
- */
-
-/**
- * @api {get} /api API index route
- * @apiGroup Index
- * @apiName APIindex
- */
-
-/**
  * Index controller
  * @param {FastifyRequest} request
  * @param {FastifyReply} reply
--- apis/index/index.js
+++ apis/index/index.js
@@ -2,6 +2,50 @@ import controller from './controller.js';
 import '../../types.js';
 
 /**
+ * @api {get} / Index route
+ * @apiGroup Index
+ * @apiName index
+ * @apiSampleRequest http://localhost:9999
+ *
+ * @apiSuccess (200) {Number} datetime Response timestamp
+ * @apiSuccess (200) {String} info Response info text
+ * @apiSuccess (200) {String} request Request path & method
+ * @apiSuccess (200) {Number} status Response status
+ * @apiSuccess (200) {Number} processingTimeMS How much time it took to process the request
+ *
+ * @apiSuccessExample {json}
+ * {
+ *   "datetime": 1703440648998,
+ *   "info": "OK",
+ *   "request": "/ [GET]",
+ *   "status": 200,
+ *   "processingTimeMS": 0
+ * }
+ */
+
+/**
+ * @api {get} /api API index route
+ * @apiGroup Index
+ * @apiName APIindex
+ * @apiSampleRequest http://localhost:9999/api
+ *
+ * @apiSuccess (200) {Number} datetime Response timestamp
+ * @apiSuccess (200) {String} info Response info text
+ * @apiSuccess (200) {String} request Request path & method
+ * @apiSuccess (200) {Number} status Response status
+ * @apiSuccess (200) {Number} processingTimeMS How much time it took to process the request
+ *
+ * @apiSuccessExample {json}
+ * {
+ *   "datetime": 1703440648998,
+ *   "info": "OK",
+ *   "request": "/api [GET]",
+ *   "status": 200,
+ *   "processingTimeMS": 0
+ * }
+ */
+
+/**
  * Register APIs
  * @param {FastifyInstance} instance
  * @returns {Promise<void>}
