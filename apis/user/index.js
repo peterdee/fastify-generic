793423f57c629123d91476ef8e05422ab49e2e9a
Peter <p.dyumin@dunice.net>
2023-12-26 15:06:32 +0300

    Work on apiDoc

--- apis/user/index.js
+++ apis/user/index.js
@@ -4,6 +4,42 @@ import schema from './schema.js';
 import '../../types.js';
 
 /**
+ * @api {GET} /api/user/:id Get user by ID
+ * @apiGroup User
+ * @apiHeader {String} Authorization API access token
+ * @apiName user
+ * @apiSampleRequest http://localhost:9999/api/user/:id
+ *
+ * @apiParam (URL parameter) {String} id User ID
+ *
+ * @apiSuccess (200) {Number} datetime Response timestamp
+ * @apiSuccess (200) {String} info Response info text
+ * @apiSuccess (200) {String} request Request path & method
+ * @apiSuccess (200) {Number} status Response status
+ * @apiSuccess (200) {Object} data Data object with user account
+ * @apiSuccess (200) {Number} processingTimeMS How much time it took to process the request
+ *
+ * @apiSuccessExample {json} 200
+ * {
+ *   "datetime": 1703440648998,
+ *   "info": "OK",
+ *   "request": "/api/user/:id [GET]",
+ *   "status": 200,
+ *   "data": {
+ *     "user": [
+ *       {
+ *         "_id": "string",
+ *         "email": "string",
+ *         "firstName": "string",
+ *         "lastName": "string"
+ *       }
+ *     ]
+ *   }
+ *   "processingTimeMS": 0
+ * }
+ */
+
+/**
  * Register APIs
  * @param {FastifyInstance} instance
  * @returns {Promise<void>}
--- apis/users/index.js
+++ apis/users/index.js
@@ -4,6 +4,49 @@ import pagination from '../../middlewares/pagination.js';
 import '../../types.js';
 
 /**
+ * @api {GET} /api/users Get users (paginated)
+ * @apiGroup Users
+ * @apiHeader {String} Authorization API access token
+ * @apiName users
+ * @apiSampleRequest http://localhost:9999/api/users
+ *
+ * @apiQuery {Number} [limit] Items per page
+ * @apiQuery {Number} [page] Page number
+ *
+ * @apiSuccess (200) {Number} datetime Response timestamp
+ * @apiSuccess (200) {String} info Response info text
+ * @apiSuccess (200) {String} request Request path & method
+ * @apiSuccess (200) {Number} status Response status
+ * @apiSuccess (200) {Object} data Data object with pagination data & user accounts
+ * @apiSuccess (200) {Number} processingTimeMS How much time it took to process the request
+ *
+ * @apiSuccessExample {json} 200
+ * {
+ *   "datetime": 1703440648998,
+ *   "info": "OK",
+ *   "request": "/api/users [GET]",
+ *   "status": 200,
+ *   "data": {
+ *     "pagination": {
+ *       "currentPage": "number",
+ *       "limit": "number",
+ *       "totalCount": "number",
+ *       "totalPages": "number"
+ *     },
+ *     "values": [
+ *       {
+ *         "_id": "string",
+ *         "email": "string",
+ *         "firstName": "string",
+ *         "lastName": "string"
+ *       }
+ *     ]
+ *   }
+ *   "processingTimeMS": 0
+ * }
+ */
+
+/**
  * Register APIs
  * @param {FastifyInstance} instance
  * @returns {Promise<void>}
