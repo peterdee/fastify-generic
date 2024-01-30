f4e2b793c03ce234a7ee2937ee7e2402d50cbde6
Peter <p.dyumin@dunice.net>
2024-01-30 14:28:39 +0300

    Fix configuration

--- configuration/index.js
+++ configuration/index.js
@@ -1,48 +1,62 @@
 import * as constants from '../constants/index.js';
 
 class Configuration {
-  static ACCESS_TOKEN_EXPIRATION_SECONDS = constants.TOKENS.access.expirationSeconds;
+  static ACCESS_TOKEN_EXPIRATION_SECONDS;
 
-  static ACCESS_TOKEN_SECRET = constants.TOKENS.access.secret;
+  static ACCESS_TOKEN_SECRET;
 
-  static APP_ENV = constants.ENVS.development;
+  static APP_ENV;
 
-  static DATABASE = {
-    connectionString: constants.DATABASE.defaultConnectionString,
-    databaseName: constants.DATABASE.defaultDatabaseName,
-  };
+  static DATABASE;
 
-  static PORT = constants.DEFAULT_PORT;
+  static PORT;
 
-  static REDIS_CONNECTION_STRING = constants.DEFAULT_REDIS_CONNECTION_STRING;
+  static REDIS_CONNECTION_STRING;
 
-  static REDIS_FLUSH_ON_STARTUP = false;
+  static REDIS_FLUSH_ON_STARTUP;
 
-  static REDIS_TEST_CONNECTION_STRING = constants.DEFAULT_REDIS_CONNECTION_STRING;
+  static REDIS_TEST_CONNECTION_STRING;
 
-  static REFRESH_TOKEN_EXPIRATION_SECONDS = constants.TOKENS.refresh.expirationSeconds;
+  static REFRESH_TOKEN_EXPIRATION_SECONDS;
 
-  static REFRESH_TOKEN_SECRET = constants.TOKENS.refresh.secret;
+  static REFRESH_TOKEN_SECRET;
 
   static #initialized = false;
 
   static init(parsed = {}) {
     if (!this.#initialized) {
-      this.ACCESS_TOKEN_EXPIRATION_SECONDS ||= Number(parsed.ACCESS_TOKEN_EXPIRATION_SECONDS);
-      this.ACCESS_TOKEN_SECRET ||= parsed.ACCESS_TOKEN_SECRET;
-      this.APP_ENV = parsed.APP_ENV || process.env.APP_ENV;
+      this.ACCESS_TOKEN_EXPIRATION_SECONDS = Number(parsed.ACCESS_TOKEN_EXPIRATION_SECONDS)
+        || constants.TOKENS.access.expirationSeconds;
+
+      this.ACCESS_TOKEN_SECRET = parsed.ACCESS_TOKEN_SECRET
+        || constants.TOKENS.access.secret;
+
+      this.APP_ENV = parsed.APP_ENV
+        || process.env.APP_ENV
+        || constants.ENVS.development;
+
       this.DATABASE = {
         connectionString: parsed.DATABASE_CONNECTION_STRING
           || constants.DATABASE.defaultConnectionString,
         databaseName: parsed.DATABASE_NAME
           || constants.DATABASE.defaultDatabaseName,
       };
-      this.PORT ||= Number(parsed.PORT);
-      this.REDIS_CONNECTION_STRING ||= parsed.REDIS_CONNECTION_STRING;
+
+      this.PORT = Number(parsed.PORT) || constants.DEFAULT_PORT;
+
+      this.REDIS_CONNECTION_STRING = parsed.REDIS_CONNECTION_STRING
+        || constants.DEFAULT_REDIS_CONNECTION_STRING;
+
       this.REDIS_FLUSH_ON_STARTUP = parsed.REDIS_FLUSH_ON_STARTUP === 'true';
-      this.REDIS_TEST_CONNECTION_STRING ||= parsed.REDIS_TEST_CONNECTION_STRING;
-      this.REFRESH_TOKEN_EXPIRATION_SECONDS ||= Number(parsed.REFRESH_TOKEN_EXPIRATION_SECONDS);
-      this.REFRESH_TOKEN_SECRET ||= parsed.REFRESH_TOKEN_SECRET;
+
+      this.REDIS_TEST_CONNECTION_STRING = parsed.REDIS_TEST_CONNECTION_STRING
+        || constants.DEFAULT_REDIS_CONNECTION_STRING;
+
+      this.REFRESH_TOKEN_EXPIRATION_SECONDS = Number(parsed.REFRESH_TOKEN_EXPIRATION_SECONDS)
+        || constants.TOKENS.refresh.expirationSeconds;
+
+      this.REFRESH_TOKEN_SECRET = parsed.REFRESH_TOKEN_SECRET
+        || constants.TOKENS.refresh.secret;
 
       this.#initialized = true;
     }
